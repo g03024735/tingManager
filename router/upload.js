9eb05bfc4cfcc63b98cb49ba2394342c66a1b30c
姜礼清 <2716855387@qq.com>
2017-10-19 16:49:51 +0800

    上传文件到OSS

--- router/upload.js
+++ router/upload.js
@@ -1,15 +1,29 @@
 const multer = require('multer')
-const upload = multer({dest: 'uploads/'})
 const request = require('request-promise')
 const sign = require('../lib/sign')
 const fs = require('fs')
 const path = require('path')
-const FormData = require('form-data')
 const {OSS} = require('../config')
+const base64url = require('base64url')
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, path.join(process.cwd(), 'uploads'))
+        },
+        filename: function (req, file, cb) {
+            cb(null, (Math.random() + "").slice(2, 15) + path.extname(file.originalname))
+        }
+    })
+})
 
 module.exports = function (app) {
     app.get('/upload', async (req, res) => {
-        res.render('upload', {})
+        const {query} = req
+        if (query.link)
+            query.link = base64url.decode(query.link)
+
+        res.render('upload', query)
     })
 
     app.post('/upload', upload.single('file'), async (req, res) => {
@@ -18,25 +32,29 @@ module.exports = function (app) {
         if (!file)
             return res.send('need file')
 
-        let {key, expire, filename} = req.body
+        //get upload url
+        let {key, expire} = req.body
         expire = parseInt(expire)
         if (isNaN(expire))
-            return res.send('expire is Nan')
+            return res.send('expire is NaN')
 
         if (expire > 0)
             expire += Date.now()
 
-        let url = `${OSS.domain}:${OSS.port}/upload?expire=${expire}&filename=${key}`
+        let url = `${OSS.domain}:${OSS.port}/upload?expire=0&filename=${key}`
         url = sign(url, OSS.secretKey).getSignHref()
 
-        let response = await request.post({
+
+        //upload oss
+        let body = await request.post({
             url,
             formData: {
-                files: fs.createReadStream(path.join(process.cwd(), file.path))
+                files: fs.createReadStream(file.path)
             }
         })
 
-        console.log(response)
-        res.send('sss')
+        //redirect upload?link=xxx
+        let accessUrl = sign(`${OSS.domain}:${OSS.port}/${key}?expire=${expire}&timestamp=${Date.now()}`, OSS.secretKey).getSignHref()
+        res.redirect(`/upload?link=${base64url(accessUrl)}`)
     })
 }
