5c60c6cbabb6c132f39a558b460fe9f5686750bd
Jack Jiang <g03024735@gmail.com>
2017-10-21 17:53:23 +0800

    修改配置

--- config/config.prod.js
+++ config/config.prod.js
@@ -1,7 +1,7 @@
 module.exports = {
     mongoUrl: 'mongodb://localhost/ting',
     OSS: {
-        domain: 'http://192.168.0.119',
+        domain: 'http://192.168.155.2',
         port: 8000,
         secretKey: '04taMi2QtiYVT7FR'
     }
