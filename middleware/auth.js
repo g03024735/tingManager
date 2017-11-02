0daba8ea065b24dfa5860bcf0415cdf3d70805d6
姜礼清 <2716855387@qq.com>
2017-11-02 15:48:37 +0800

    登录

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,4 @@
 module.exports = function (req, res, next) {
-    console.log(req.session.user, req.url)
     if(!req.session.user && req.url.indexOf('/login') === -1)
         return res.redirect('/login')
     next();
